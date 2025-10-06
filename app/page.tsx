c19177a2846c44b75babcdc06bbf26def497e311
Shankar <72694164+shankarchandran21@users.noreply.github.com>
2025-10-06 11:27:34 +0530

    folder-setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Click Me</Button>
+    </div>
+  )
+}
+
+export default Home
